e52f33477ec991beb1e607715735d7901055beb9
Raj Shah <raj.shah@ge.com>
2020-05-29 03:50:05 +0530

    client can set props, replace multiple sets with mset, modified as middleware

--- rateLimiter.js
+++ rateLimiter.js
@@ -1,46 +1,59 @@
 let rateLimiter = {};
 let redis = require('redis');
 let dayjs = require('dayjs');
-let now = dayjs();
-const redisCredentials = require('./redisCredentials.json');
-const timeFormat = 'DD/MM/YY HH:mm:ss';
-const maxHits = 5;
-const window = 60000;
-client = redis.createClient(redisCredentials);
-// multi = client.multi();
 
-rateLimiter.limit = function (id) {
+options = {
+    redisCredentials: {"port": 6379,"host": "0.0.0.0"},
+    timeFormat: 'DD/MM/YY HH:mm:ss',
+    limit: 5,
+    window: 60000,
+    statusCode: 429,
+    message: 'Amount of requests beyond limit of user',
+    keyFunction: function(req,res) {return req.ip}
+};
+
+rateLimiter.setOptions = function(clientOptions) {
+    options = Object.assign(options, clientOptions);
+    client = redis.createClient(options.redisCredentials);
+};
+
+rateLimiter.limit = function (req, res, next) {
+    let id = options.keyFunction(req,res);
     let timerId = 'time-'+id;
     let counterId = 'count-'+id;
     client.get(timerId,function(err, value) {
         if (err) { 
-            throw err;
+            next(err);
         } else {
-            console.log(dayjs(value).format(timeFormat));
-            if (value == null || dayjs().valueOf() - value > window) {
-                client.set(timerId, dayjs().valueOf(),function(err) {
+            console.log(dayjs(value).format(options.timeFormat));
+            if (value == null || dayjs().valueOf() - value > options.window) {
+                let arr = [];
+                arr.push(timerId);
+                arr.push(dayjs().valueOf());
+                arr.push(counterId);
+                arr.push(1);
+                client.mset(arr, function(err) {
                     if (err) {
-                        throw err;
-                    }
-                });
-                client.set(counterId, 1, function(err) {
-                    if (err) {
-                        throw err;
+                        next(err);
+                    } else {
+                        next();
                     }
                 });
             } else {
                 client.get(counterId, function(err, count) {
                     count = parseInt(count);
                     if (err) {
-                        throw err;
+                        next(err);
                     } else {
                         console.log("count " + count);
-                        if (count == maxHits) {
-                            console.log("throttling client");
+                        if (count == options.limit) {
+                            res.status(options.statusCode).send(options.message);
                         } else {
                             client.set(counterId, count + 1, function(err) {
                                 if (err) {
-                                    throw err;
+                                    next(err);
+                                } else {
+                                    next();
                                 }
                             });
                         }
